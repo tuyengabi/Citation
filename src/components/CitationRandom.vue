f8b22252d34e9099caa0661f09bb12df5e801c0a
tuyengabi <tuyen.gabi@gmail.com>
2019-05-04 19:29:50 +0200

    add photos and file CitationRandom , modify file App

---
+++ src/components/CitationRandom.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="page">
+    <img class="img" src="adress">
+    <p class="qte">{{ quote }}</p>
+    <p class="atr">{{ author }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CitationRandom',
+    data() {
+      return {
+        quotes: [
+          {
+            adress: "../assets/img01.jpg",
+            author: "Albert Einstein",
+            quote: "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.",
+          },
+
+          {
+            adress: "../assets/img02.jpg",
+            author: "Albert Einstein",
+            quote: "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.",
+          },
+
+          {
+            adress: "../assets/img03.jpg",
+            author: "Albert Einstein",
+            quote: "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.",
+          },
+
+          {
+            adress: "../assets/img04.jpg",
+            author: "Albert Einstein",
+            quote: "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.",
+          },
+
+          {
+            adress: "../assets/img05.jpg",
+            author: "Albert Einstein",
+            quote: "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.",
+          },
+
+          {
+            adress: "../assets/img06.jpg",
+            author: "Albert Einstein",
+            quote: "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.",
+          },
+
+          {
+            adress: "../assets/img1.jpg",
+            author: "Albert Einstein",
+            quote: "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.",
+          },
+        ]
+    }
+  }
+</script>
+
+<style>
+
+</style>
